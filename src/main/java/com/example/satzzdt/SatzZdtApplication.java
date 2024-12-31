a2f7c45e7644755de5bc2dd555707ed2b05ae349
Lim jeong woo <dlawjddn0105@gmail.com>
2024-12-31 15:11:30 +0900

    first commit

---
+++ src/main/java/com/example/satzzdt/SatzZdtApplication.java
@@ -0,0 +1,13 @@
+package com.example.satzzdt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SatzZdtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SatzZdtApplication.class, args);
+	}
+
+}
